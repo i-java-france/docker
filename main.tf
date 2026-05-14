c70fd99827bcf34422b83a55419ccf18ee798e50
-i-java <abdelhadieddiraa4@gmail.com>
2026-05-14 18:29:46 +0100

    fix same isuus

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.54.0"
+      version = "~> 1.54.0"
     }
   }
 }
 
 variable "hcloud_token" {
-  description = "HmuNE2KcDOgkEtnj5XHdVVHyZOVFQUBErM0FNGDFTz3e1elHzKwZ2CnxAtarX7aS"
+  description = "Hetzner Cloud API token"
   type        = string
   sensitive   = true
 }
@@ -40,20 +40,28 @@ resource "hcloud_ssh_key" "dokploy_ssh_key" {
   public_key = var.ssh_public_key
 }
 
-data "hcloud_primary_ip" "dokploy-ip" {
-  name = "primary_ip-1"
+# Use the existing primary IP
+data "hcloud_primary_ip" "dokploy_ip" {
+  name = "primary_ip-1"  # Changed to match your actual IP name
 }
 
 resource "hcloud_server" "dokploy" {
   name         = "dokploy"
-  server_type  = "cx23"
+  server_type  = "cx22"  # Changed from cx23 (doesn't exist) to cx22
   image        = "docker-ce"
   location     = "fsn1"
   ssh_keys     = [hcloud_ssh_key.dokploy_ssh_key.id]
-  firewall_ids = [] # add your firewall ID's here to apply during provisioning
+  firewall_ids = []
 
   public_net {
-    ipv4 = data.hcloud_primary_ip.dokploy-ip.id
+    ipv4_enabled = true
+    ipv4         = data.hcloud_primary_ip.dokploy_ip.id
+    ipv6_enabled = true
+  }
+
+  # Wait for server to be ready
+  provisioner "local-exec" {
+    command = "sleep 30"
   }
 
   # Create directory first
@@ -63,6 +71,7 @@ resource "hcloud_server" "dokploy" {
       user        = "root"
       private_key = var.ssh_private_key
       host        = self.ipv4_address
+      timeout     = "5m"
     }
     inline = [
       "mkdir -p /opt/dokploy"
@@ -76,23 +85,37 @@ resource "hcloud_server" "dokploy" {
       user        = "root"
       private_key = var.ssh_private_key
       host        = self.ipv4_address
+      timeout     = "5m"
     }
-    source      = "./ssh_init.sh"
+    source      = "${path.module}/ssh_init.sh"
     destination = "/opt/dokploy/ssh_init.sh"
   }
 
+  # Execute installation
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
       user        = "root"
       private_key = var.ssh_private_key
       host        = self.ipv4_address
+      timeout     = "10m"
     }
 
     inline = [
+      "chmod +x /opt/dokploy/ssh_init.sh",
       "sh /opt/dokploy/ssh_init.sh",
       "rm -rf /opt/dokploy",
       "curl -sSL https://dokploy.com/install.sh | sh"
     ]
   }
+}
+
+output "server_ip" {
+  description = "The public IPv4 address of the Dokploy server"
+  value       = hcloud_server.dokploy.ipv4_address
+}
+
+output "dokploy_url" {
+  description = "URL to access Dokploy"
+  value       = "http://${hcloud_server.dokploy.ipv4_address}:3000"
 }
